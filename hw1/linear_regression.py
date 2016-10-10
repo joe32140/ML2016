82bf1b529cf638d9486a8731973fa864f324d906
joe32140 <joe32140@hotmail.com.tw>
2016-10-10 21:19:37 +0800

    no direction =(

--- hw1/linear_regression.py
+++ hw1/linear_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-numberofiter = 2000
+numberofiter = 1
 
 data_path = 'data/train.csv'
 test_data = 'data/test_X.csv'
@@ -16,9 +16,10 @@ for i in range(240):
 for i in range(5760):
     if math.isnan(x[10, i]):
         x[10, i] = float(0)
-x_scale = (sum(x.T)/5760).reshape((18, 1));
-print x_scale.shape
-print x_scale
+#x_scale = (sum(x.T)/5760).reshape((18, 1));
+#x = (x.T/x_scale.T).T
+#print x_scale.shape
+#print x_scale
 #print x[9, :]
 
 test_data = np.genfromtxt (test_data, delimiter=",")
@@ -30,26 +31,36 @@ for i in range(240):
 #print test_data[10]
 #print x[9, :]
 # Gradient Descend
-theta = np.zeros(18*9).reshape((18, 9))
+theta = np.zeros(18*9).reshape((18*9, 1))
 bias = 0.0
-alpha = 0.000002
-for i in range(numberofiter):
-    for j in range(5750):#5750
-        hypothesis =sum(sum(x[:, j:j+9]*theta)) + bias
+alpha = 0.000001
+last_loss = 0.0
+converge = False
+it = 0
+while not converge:
+    for j in range(500):#5750
+        hypothesis =np.dot(x[:, j:j+9].reshape((1, 18*9)),theta) + bias
         #print x[:, :9]
+        print hypothesis.shape
         loss =  hypothesis - x[9, j+9]
-        #print("Iteration %d | Cost: %f" % (j, loss))
-        gradient = x[:, j:j+9]*loss
-        theta = theta - alpha*gradient/x_scale
+        print("Iteration %d | Cost: %f" % (j, loss))
+        gradient = x[:, j:j+9].reshape((18*9, 1))*loss
+        #print gradient
+        theta = theta - alpha*gradient
         bias = bias - alpha*loss
+    it = it + 1
+    if loss**2 < 5 or it >= numberofiter:
+        converge = True  
 
 # Testing
-print theta
-print bias
+#print theta
+#print bias
 ans = open('ans.csv', 'w+')
 ans.write('id,value\n')
 for i in range(240):
-    predict = sum(sum(test_data[i:i+18, :]*theta)) + bias
+    #query = (test_data[i:i+18, :].T/x_scale.T).T
+    #predict = sum(sum(query*theta)) + bias
+    predict = np.dot(test_data[i:i+18, :].reshape(1, 18*9), theta) + bias
     #print test_data[i:i+18, :]
     #print predict
     ans.write('id_%d,%f\n' %(i, predict))
