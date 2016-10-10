c9292c2b2ea1412277420e7b0cb197b08e473bdf
joe32140 <joe32140@hotmail.com.tw>
2016-10-10 20:09:44 +0800

    tune feature

--- hw1/linear_regression.py
+++ hw1/linear_regression.py
@@ -1,7 +1,7 @@
 import numpy as np
 import math
 
-numberofiter = 8
+numberofiter = 2000
 
 data_path = 'data/train.csv'
 test_data = 'data/test_X.csv'
@@ -16,30 +16,31 @@ for i in range(240):
 for i in range(5760):
     if math.isnan(x[10, i]):
         x[10, i] = float(0)
+x_scale = (sum(x.T)/5760).reshape((18, 1));
+print x_scale.shape
+print x_scale
 #print x[9, :]
 
 test_data = np.genfromtxt (test_data, delimiter=",")
 test_data = test_data[:, 2:]
-print test_data.shape
 for i in range(240):
     for j in range(9):
         if math.isnan(test_data[i*18+10, j]):
             test_data[i*18+10, j] = float(0)
 #print test_data[10]
-print x[9, :]
+#print x[9, :]
 # Gradient Descend
-theta = np.ones(18*9).reshape((18, 9))
+theta = np.zeros(18*9).reshape((18, 9))
 bias = 0.0
-alpha = 0.000001
-print x[:, :9].shape
+alpha = 0.000002
 for i in range(numberofiter):
     for j in range(5750):#5750
         hypothesis =sum(sum(x[:, j:j+9]*theta)) + bias
         #print x[:, :9]
         loss =  hypothesis - x[9, j+9]
-        print("Iteration %d | Cost: %f" % (j, loss))
+        #print("Iteration %d | Cost: %f" % (j, loss))
         gradient = x[:, j:j+9]*loss
-        theta = theta - alpha*gradient
+        theta = theta - alpha*gradient/x_scale
         bias = bias - alpha*loss
 
 # Testing
