3eb47797d78d26a98a0ceb3860aa595933a69820
徐朝駿 <joe32140@hao-login.localdomain>
2016-12-21 10:31:04 +0800

    QQ

--- hw2/test.py
+++ hw2/test.py
@@ -5,6 +5,7 @@ model_name = sys.argv[1]
 model = np.loadtxt(model_name)
 print (model.shape)
 theta = model[:57]
+print (theta.shape)
 bias = model[57]
 test_data = np.genfromtxt (sys.argv[2], delimiter=",")
 test_data = test_data[:, 1:]
@@ -13,6 +14,7 @@ ans = open(sys.argv[3], 'w+')
 ans.write('id,label\n')
 for i in range(test_data.shape[0]):
     predict = np.dot(test_data[i].reshape((1, 57)), theta) + bias
+    print (predict.shape)
     if predict >= 0.5:
         predict = 1
     else :
